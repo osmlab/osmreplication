2ac7852035a97c1bebb5e30028dcf2c73960cbb1
Rub21 <ruben@mapbox.com>
2016-01-28 08:53:12 -0500

    time --> Date

--- index.js
+++ index.js
@@ -2,25 +2,25 @@
 
 var date2osmdiffs = {
   minute: function(date) {
-    var startTimeMinute = 1453907940; //01/27/2016 @ 3:19pm
+    var startDateMinute = 1453907940; //01/27/2016 @ 3:19pm
     var startNumberMinute = 1766000; //minute/001/766/000.osc.gz
     var intervalMinute = 60;
     var valueTimestamp = timestamp(date);
 
     valueTimestamp = valueTimestamp - valueTimestamp % 10;
-    var diffTime = valueTimestamp - startTimeMinute;
-    var number = diffTime / intervalMinute;
+    var diffDate = valueTimestamp - startDateMinute;
+    var number = diffDate / intervalMinute;
     return parseInt(startNumberMinute + number);
   },
   hour: function(date) {
-    var startTimeHour = 1451833200; //01/03/2016 @ 3:00pm
+    var startDateHour = 1451833200; //01/03/2016 @ 3:00pm
     var startNumberHour = 29000; //hour/000/029/000.osc.gz
     var intervalHour = 3600;
     var valueTimestamp = timestamp(date);
 
     valueTimestamp = valueTimestamp - valueTimestamp % 100;
-    var diffTime = valueTimestamp - startTimeHour;
-    var number = diffTime / intervalHour;
+    var diffDate = valueTimestamp - startDateHour;
+    var number = diffDate / intervalHour;
     return parseInt(startNumberHour + number);
   }
 };
